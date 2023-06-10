61f10962202c74a9139e04599f72ae9b508549ca
Musiker15 <info@musiker15.de>
2023-06-10 21:41:42 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -182,7 +182,7 @@ GithubUpdater = function()
                 print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
             elseif CurrentVersion ~= NewestVersion then
                 print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
-                print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://keymaster.fivem.net/^0')
+                print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://github.com/MSK-Scripts/msk_saveCoordMenu^0')
             end
             print("###############################")
         end)
